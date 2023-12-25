c6a11a8cdce1a46ea83892073821f53e7cc38d01
Adesina <adeshiname@gmail.com>
2023-12-25 15:43:20 +0100

    routes updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
-        path:'', redirectTo:'login', pathMatch:'full',
+        path:'', redirectTo:'home', pathMatch:'full',
+    },
+    {
+        path:'home', component:HomeComponent,
     },
     {
         path:'login', component:LoginComponent
